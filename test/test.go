30f1688c8d819be4af0b547349bca60f16f8a182
wxl <wxl200854@126.com>
2019-03-05 15:52:21 +0800

    test 2019-3-5 15:52

--- test/test.go
+++ test/test.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello, world!")
+func main() {
+	var a int
+	var b string
+	fmt.Println("a = ", a)
+	a = 10
+	b = "welcome"
+	fmt.Println("a = ", a)
+	fmt.Println("b = ", b)
+
+	c := 125
+	c = 300
+	fmt.Printf("a = %d, b = %s, c = %d", a, b, c)
 }
